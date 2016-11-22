fa644a2c8bdc5ff3ce87ef17688fbc6ba5f75a0d
Santiago Dueñas <sduenas@bitergia.com>
2016-11-22 17:17:53 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#     Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
+#
+
+import codecs
+import os.path
+import re
+
+from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme_md = os.path.join(here, 'README.md')
+
+# Pypi wants the description to be in reStrcuturedText, but
+# we have it in Markdown. So, let's convert formats.
+# Set up thinkgs so that if pypandoc is not installed, it
+# just issues a warning.
+try:
+    import pypandoc
+    long_description = pypandoc.convert(readme_md, 'rst')
+except (IOError, ImportError):
+    print("Warning: pypandoc module not found, or pandoc not installed. " \
+          + "Using md instead of rst")
+    with codecs.open(readme_md, encoding='utf-8') as f:
+        long_description = f.read()
+
+
+version = '0.1.0.dev0'
+
+
+setup(name="perceval-mozilla",
+      description="Bundle of Perceval backends for Mozilla ecosystem",
+      long_description=long_description,
+      url="https://github.com/grimoirelab/perceval-mozilla",
+      version=version,
+      author="Bitergia",
+      author_email="sduenas@bitergia.com",
+      license="GPLv3",
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3'
+      ],
+      keywords="development repositories analytics mozilla kitsune remo",
+      packages=[
+        'perceval',
+        'perceval.backends',
+        'perceval.backends.mozilla'
+      ],
+      namespaces=[
+        'perceval.backends'
+      ],
+      install_requires=[
+        'requests>=2.7.0',
+        'perceval>0.4'
+      ],
+      zip_safe=False)
