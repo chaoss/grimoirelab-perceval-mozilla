45cd10ce5005b7c5a27e2f287920775d5a670ed0
Santiago Dueñas <sduenas@bitergia.com>
2017-03-06 12:59:02 +0100

    [tests] Fix whitespace/black lines PEP8 code style errors

--- tests/test_kitsune.py
+++ tests/test_kitsune.py
@@ -55,6 +55,7 @@ KITSUNE_API_ANSWER = KITSUNE_SERVER_URL + '/api/2/answer/'
 KITSUNE_SERVER_FAIL_PAGE = 69
 KITSUNE_ITEMS_PER_PAGE = 20
 
+
 def read_file(filename, mode='r'):
     with open(filename, mode) as f:
         content = f.read()
@@ -218,7 +219,7 @@ class TestKitsuneBackend(unittest.TestCase):
         self.assertEqual(len(questions), 0)
 
         # Get no questions: we have two pages
-        offset = KitsuneClient.ITEMS_PER_PAGE*2
+        offset = KitsuneClient.ITEMS_PER_PAGE * 2
         with self.assertRaises(requests.exceptions.HTTPError):
             questions = [question for question in kitsune.fetch(offset=offset)]
             self.assertEqual(len(questions), 0)
@@ -279,7 +280,7 @@ class TestKitsuneBackendCache(unittest.TestCase):
         self.assertEqual(len(HTTPServer.requests_http), requests_done)
         # The contents should be the same
         self.assertEqual(len(cached_questions), len(questions))
-        for i in range(0,len(questions)):
+        for i in range(0, len(questions)):
             self.assertDictEqual(cached_questions[i]['data'], questions[i]['data'])
 
     def test_fetch_from_empty_cache(self):
@@ -347,15 +348,15 @@ class TestKitsuneClient(unittest.TestCase):
         # Set up a mock HTTP server
         body = read_file('data/kitsune/kitsune_questions_1_2.json')
         client = KitsuneClient(KITSUNE_SERVER_URL)
-        response = next(client.get_questions()) # first group of questions
+        response = next(client.get_questions())  # first group of questions
         req = HTTPServer.requests_http[-1]
         self.assertEqual(response, body)
         self.assertEqual(req.method, 'GET')
         self.assertRegex(req.path, '/api/2/question/')
         # Check request params
         expected = {
-                    'page' : ['1'],
-                    'ordering' : ['updated'],
+                    'page': ['1'],
+                    'ordering': ['updated'],
                     }
         self.assertDictEqual(req.querystring, expected)
 
@@ -377,8 +378,8 @@ class TestKitsuneClient(unittest.TestCase):
         # Check request params
         expected = {
                     'question': ['1129949'],
-                    'page' : ['1'],
-                    'ordering' : ['updated']
+                    'page': ['1'],
+                    'ordering': ['updated']
                     }
         self.assertDictEqual(req.querystring, expected)
 
